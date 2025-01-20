8f39a464b370786f327e649f6574ae78c2c1ee85
ayushagarwal2502 <159728868+ayushagarwal2502@users.noreply.github.com>
2025-01-20 23:41:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	getMakeModel = function() {
+    return this.make + " " + this.model;
+}
+}
 
-function SportsCar(make, model, topSpeed) {}
+function SportsCar(make, model, topSpeed) {
+	 getTopSpeed(){
+		 return this.topSpeed
+	 }
+}
 
 // Do not change the code below
 window.Car = Car;
